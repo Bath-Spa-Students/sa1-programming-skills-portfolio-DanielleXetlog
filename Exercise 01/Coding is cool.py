fbfdd83f5893c6fa86e90b5c9d7f5add760c2150
Danielle <daniellegatan@gmail.com>
2024-10-17 11:15:10 +0400

    Coding is cool

---
+++ Exercise 01/Coding is cool.py
@@ -0,0 +1,10 @@
+# In this excersise I've made 3 ways to print "Coding is cool"
+# Define the words to be concatenated
+word1 = "Coding "
+word2 = "is "
+word3 = "cool"
+
+# Concatenate the words and print the result
+# The '+' operator is used to join the strings together
+print(word1 + word2 + word3)
+
