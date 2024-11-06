b863265ac1a008d65636f153d0d1022bc42cef37
Danielle <daniellegatan@gmail.com>
2024-11-06 13:14:28 +0400

    Advanced Requirement
    Exercise 4 Primitive Quiz - Advanced Requirement

---
+++ Exercise 04/Advanced Requirements.py
@@ -0,0 +1,131 @@
+"""
+Advanced Requirements:
+Ignore Capitalization: Modify your program to accept answers regardless of
+the capitalization (e.g., “paris”, “Paris”, and “PaRis” should all be considered
+correct). Multiple Questions: Extend the program into a quiz that asks for the
+capitals of 10 European countries. Provide feedback for each question.
+"""
+# Request the user input the capital of France.
+Capital_France = input('/n What is the Capital of France? ').title()
+# if checks if the answer is correct and prints a confirmation message below the answer.
+if Capital_France.title() == 'Paris':
+    print(f'Correct the answer is {Capital_France}!!!')
+# If the user didnt type anything, it will print out a message that no answer was provided.
+elif Capital_France == '':
+    print('You did not enter any answer.')
+# If the answer is wrong it will print out "Your answer is wrong."
+else:
+    print(f"Your answer is wrong.")
+
+# Request the user input the capital of Croatia.
+Capital_Croatia = input('/n What is the Capital of Croatia? ').title()
+# if checks if the answer is correct and prints a confirmation message below the answer.
+if Capital_Croatia.title() == 'Zagreb':
+    print(f'Correct the answer is {Capital_Croatia}!!!')
+# If the user didnt type anything, it will print out a message that no answer was provided.
+elif Capital_Croatia == '':
+    print('You did not enter any answer.')
+# If the answer is wrong it will print out "Your answer is wrong."
+else:
+    print(f"Your answer is wrong.")
+
+# Request the user input the capital of Finland.
+Capital_Finland = input('/n What is the Capital of Finland? ').title()
+# if checks if the answer is correct and prints a confirmation message below the answer.
+if Capital_Finland.title() == 'Helsinki':
+    print(f'Correct the answer is {Capital_Finland}!!!')
+# If the user didnt type anything, it will print out a message that no answer was provided.
+elif Capital_Finland == '':
+    print('You did not enter any answer.')
+# If the answer is wrong it will print out "Your answer is wrong."
+else:
+    print(f"Your answer is wrong.")
+    
+# Request the user input the capital of Luxembourg.
+Capital_Luxembourg = input('/n What is the Capital of Luxembourg? ').title()
+# if checks if the answer is correct and prints a confirmation message below the answer.
+if Capital_Luxembourg.title() == 'Luxembourg City':
+    print(f'Correct the answer is {Capital_Luxembourg}!!!')
+# If the user didnt type anything, it will print out a message that no answer was provided.
+elif Capital_Luxembourg == '':
+    print('You did not enter any answer.')
+# If the answer is wrong it will print out "Your answer is wrong."
+else:
+    print(f"Your answer is wrong.")
+    
+# Request the user input the capital of Latvia.
+Capital_Latvia = input('/n What is the Capital of Latvia? ').title()
+# if checks if the answer is correct and prints a confirmation message below the answer.
+if Capital_Latvia.title() == 'Riga':
+    print(f'Correct the answer is {Capital_Latvia}!!!')
+# If the user didnt type anything, it will print out a message that no answer was provided.
+elif Capital_Latvia == '':
+    print('You did not enter any answer.')
+# If the answer is wrong it will print out "Your answer is wrong."
+else:
+    print(f"Your answer is wrong.")
+    
+# Request the user input the capital of Portugal.
+Capital_Portugal = input('/n What is the Capital of Portugal? ').title()
+# if checks if the answer is correct and prints a confirmation message below the answer.
+if Capital_Portugal.title() == 'Lisbon':
+    print(f'Correct the answer is {Capital_Portugal}!!!')
+# If the user didnt type anything, it will print out a message that no answer was provided.
+elif Capital_Portugal == '':
+    print('You did not enter any answer.')
+# If the answer is wrong it will print out "Your answer is wrong."
+else:
+    print(f"Your answer is wrong.")
+    
+# Request the user input the capital of United Kingdom.
+Capital_United_Kingdom = input('/n What is the Capital of United Kingdom? ').title()
+# if checks if the answer is correct and prints a confirmation message below the answer.
+if Capital_United_Kingdom.title() == 'London':
+    print(f'Correct the answer is {Capital_United_Kingdom}!!!')
+# If the user didnt type anything, it will print out a message that no answer was provided.
+elif Capital_United_Kingdom == '':
+    print('You did not enter any answer.')
+# If the answer is wrong it will print out "Your answer is wrong."
+else:
+    print(f"Your answer is wrong.")
+    
+# Request the user input the capital of Denmark.
+Capital_Denmark = input('/n What is the Capital of Denmark? ').title()
+# if checks if the answer is correct and prints a confirmation message below the answer.
+if Capital_Denmark.title() == 'Copenhagen':
+    print(f'Correct the answer is {Capital_Denmark}!!!')
+# If the user didnt type anything, it will print out a message that no answer was provided.
+elif Capital_Denmark == '':
+    print('You did not enter any answer.')
+# If the answer is wrong it will print out "Your answer is wrong."
+else:
+    print(f"Your answer is wrong.")
+    
+# Request the user input the capital of Romania.
+Capital_Romania = input('/n What is the Capital of Romania? ').title()
+# if checks if the answer is correct and prints a confirmation message below the answer.
+if Capital_Romania.title() == 'Bucharest':
+    print(f'Correct the answer is {Capital_Romania}!!!')
+# If the user didnt type anything, it will print out a message that no answer was provided.
+elif Capital_Romania == '':
+    print('You did not enter any answer.')
+# If the answer is wrong it will print out "Your answer is wrong."
+else:
+    print(f"Your answer is wrong.")
+    
+# Request the user input the capital of Poland.
+Capital_Poland = input('/n What is the Capital of Poland? ').title()
+# if checks if the answer is correct and prints a confirmation message below the answer.
+if Capital_Poland.title() == 'Warsaw':
+    print(f'Correct the answer is {Capital_Poland}!!!')
+# If the user didnt type anything, it will print out a message that no answer was provided.
+elif Capital_Poland == '':
+    print('You did not enter any answer.')
+# If the answer is wrong it will print out "Your answer is wrong."
+else:
+    print(f"Your answer is wrong.")
+    
+# title is used for making the first letter of the word a capital letter.
+# if statement is used for checking if the answer matches the correct answer.
+# elif statement is used for catching the case and prints a message indivating that there was no answer written.
+# else statement is used for incorrect answers and printing a message telling the user is wrong.
