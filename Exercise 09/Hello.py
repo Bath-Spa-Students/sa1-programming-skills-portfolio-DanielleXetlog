1c80936348f3de07cc561a1f02bd4c72278e7312
Danielle <daniellegatan@gmail.com>
2024-11-13 14:26:59 +0400

    exercise 9
    Exercise 9 - Hello

---
+++ Exercise 09/Hello.py
@@ -0,0 +1,27 @@
+"""
+Fill in the blanks in the code below so that the function hello() prints "Hello" to the console.
+```python
+
+def hello():
+    ____  # Fill in this blank to print "Hello" to the console
+
+def main():
+    ____  # Fill in this blank to call the hello() function
+
+if __name__ == "__main__":
+    main()
+"""
+
+# Defines a function named hello.
+def hello():
+    print("Hello")
+# Defines the main function.
+def main():
+    # Calls the display function.
+    hello()
+
+# Checks if the code is being run directly.
+if __name__ == "__main__":
+    # Calls the main function.
+    main()
+    
